b2b97128c98457897dc5bf05b85b3cbb7d15eace
Piet Geursen <piet.geursen@gmail.com>
2025-05-05 21:25:54 +1200

    make some suggestions

--- src/go_set/go_set_claim.rs
+++ src/go_set/go_set_claim.rs
@@ -1,20 +1,18 @@
 use crate::feed_id::FeedId;
-use crate::go_set::{GOSet, GOSetXor};
-
-use crate::Error;
+use crate::go_set::GOSetXor;
 
 #[derive(Debug, PartialEq)]
-enum GOSetClaimError {
+pub enum GOSetClaimError {
     InvalidDMX,
     InvalidTypeCode,
 }
 
 #[derive(Debug)]
 pub struct GOSetClaim {
-    lowest_feed_id: FeedId,
-    highest_feed_id: FeedId,
-    xor: GOSetXor,
-    count: u8,
+    pub(crate) lowest_feed_id: FeedId,
+    pub(crate) highest_feed_id: FeedId,
+    pub(crate) xor: GOSetXor,
+    pub(crate) count: u8,
 }
 
 impl GOSetClaim {
@@ -25,21 +23,29 @@ impl GOSetClaim {
     // GOSET_DMX = first 7 bytes of SHA256(GOSET_DMX_MATERIAL)
     // ```
 
-    pub fn encode(go_set: &GOSet) -> [u8; 105] {
+    pub fn encode(&self) -> [u8; 105] {
         // NOTE: we're gonna want to make a range of claims
         // 1. how do we deal with the concept of a "subset" of a claim?
+        // PG: I think that GoSets should implement methods that let you create new sets from
+        // themselves. So GoSet could have a `bisect(&self) -> GoSet` method. If bisect is the
+        // right word
         // 2. where does the logic live about comparing a claim with a goset?
+        // PG: I implemented PartialEq on GoSet where the right hand side is GoSetClaim.
         // 3. what's the algorithm for the claim-dance?
+        // PG: That's gonna live in some new type we haven't made yet
+        //
+        // PG: I think that if GoSet.lowest_feed_id returns an option then GoSetClaim
+        // lowest_feed_id should also be an option. Same for highest obvs.
 
         // [DMX (7B) | 'c' (1 byte) | lowest FeedId (32 bytes) | highest FeedId (32 bytes) | XOR (32 bytes) | cnt (1 byte) ]
         let mut claim = [0; 105];
 
         let dmx = Self::GOSET_DMX;
         let type_code: [u8; 1] = [b'c'];
-        let lowest_feed_id = go_set.lowest_feed_id().unwrap().encode();
-        let highest_feed_id = go_set.highest_feed_id().unwrap().encode();
-        let xor = go_set.xor().encode();
-        let count: [u8; 1] = [go_set.count()];
+        let lowest_feed_id = self.lowest_feed_id.encode();
+        let highest_feed_id = self.highest_feed_id.encode();
+        let xor = self.xor.encode();
+        let count: [u8; 1] = [self.count];
 
         let chunks: [&[u8]; 6] = [
             &dmx,
@@ -105,18 +111,20 @@ impl GOSetClaim {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use crate::go_set::GOSet;
 
     #[test]
     fn claim_round_trip() {
         let feed_a = FeedId([255; 32]);
         let feed_b = FeedId([1; 32]);
         let feed_c = FeedId([2; 32]);
-        let go_set = GOSet::new(&[feed_a, feed_b, feed_c]);
+        let go_set = GOSet::new(&[feed_a, feed_b, feed_c]).unwrap();
 
-        let claim = GOSetClaim::encode(&go_set);
+        let claim = go_set.create_claim().unwrap();
+        let encoded_claim = claim.encode();
         println!("encoded claim:\n {:?}", claim);
 
-        let result = GOSetClaim::decode(&claim).unwrap();
+        let result = GOSetClaim::decode(&encoded_claim).unwrap();
         println!("decoded claim:\n {:?}", result);
 
         assert_eq!(&result.lowest_feed_id, go_set.lowest_feed_id().unwrap());
@@ -128,14 +136,15 @@ mod tests {
     #[test]
     fn claim_wrong_dmx() {
         let feed_a = FeedId([255; 32]);
-        let go_set = GOSet::new(&[feed_a]);
+        let go_set = GOSet::new(&[feed_a]).unwrap();
 
-        let mut claim = GOSetClaim::encode(&go_set);
+        let claim = go_set.create_claim().unwrap();
+        let mut encoded_claim = claim.encode();
         let wrong_dmx = [6; 7];
-        claim[0..7].copy_from_slice(&wrong_dmx);
+        encoded_claim[0..7].copy_from_slice(&wrong_dmx);
         println!("claim with incorrect dmx:\n {:?}", claim);
 
-        match GOSetClaim::decode(&claim) {
+        match GOSetClaim::decode(&encoded_claim) {
             Err(GOSetClaimError::InvalidDMX) => {} // passed
             _ => panic!("Expected InvalidDMX error"),
         }
@@ -144,14 +153,15 @@ mod tests {
     #[test]
     fn claim_wrong_type_code() {
         let feed_a = FeedId([255; 32]);
-        let go_set = GOSet::new(&[feed_a]);
+        let go_set = GOSet::new(&[feed_a]).unwrap();
 
-        let mut claim = GOSetClaim::encode(&go_set);
+        let claim = go_set.create_claim().unwrap();
+        let mut encoded_claim = claim.encode();
         let wrong_type_code = [b'W'];
-        claim[7..8].copy_from_slice(&wrong_type_code);
+        encoded_claim[7..8].copy_from_slice(&wrong_type_code);
         println!("claim with incorrect type_code:\n {:?}", claim);
 
-        match GOSetClaim::decode(&claim) {
+        match GOSetClaim::decode(&encoded_claim) {
             Err(GOSetClaimError::InvalidTypeCode) => {} // passed
             _ => panic!("Expected InvalidTypeCode error"),
         }
